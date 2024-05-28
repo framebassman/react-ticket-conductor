0010d51c539a9c78098dcba27fa3cb7ea9e80f55
Dmitry Romashov <framebassman@gmail.com>
2024-05-28 18:10:55 +0200

    Code cleanup

--- src/reportWebVitals.ts
+++
@@ -1,9 +0,0 @@
-import { onCLS, onINP, onLCP } from 'web-vitals';
-
-const reportWebVitals = () => {
-  onCLS(console.log);
-  onINP(console.log);
-  onLCP(console.log);
-};
-
-export default reportWebVitals;
